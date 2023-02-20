c2b58e26801b5795684f36350a0a42def896c2ec
KOCNS1 <enes1.koc@outlook.com>
2023-02-20 02:11:45 +0100

    :ambulance: [FIX] gh-pages

--- src/navigation/router.tsx
+++ src/navigation/router.tsx
@@ -4,19 +4,22 @@ import App from "../App";
 import Home from "../pages/Home/Home";
 import Results from "../pages/Results/Results";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/results/:teamCode",
-        element: <Results />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/results/:teamCode",
+          element: <Results />,
+        },
+      ],
+    },
+  ],
+  { basename: "/react-cert-2" }
+);
